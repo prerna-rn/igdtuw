13423bcb7f4cf8ca8da4582cfb5f525012932fb4
prerna-rn <prerna194btcse21@igdtuw.ac.in>
2024-02-12 00:37:23 +0530

    try login redirect

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ To-do <br>
 ☑️ sign up, login page <br>
 🔳 store credentials in php <br>
 🔳 after successful login, connect to the erp dashboard <br>
-🔳 chatbot <br>
+☑️ chatbot <br>
 ☑️ awards and achievements carousel <br>
 ☑️ add light dark toggler <br>
 🔳 add light, dark mode colors <br>
--- login/login.js
+++ login/login.js
@@ -4,8 +4,10 @@ const container = document.getElementById('container');
 
 signUpButton.addEventListener('click', () => {
 	container.classList.add("right-panel-active");
+	window.location.href = "/dashboard/index.html";
 });
 
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
+	window.location.href = "/dashboard/index.html";
 });
